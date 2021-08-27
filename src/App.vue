0247cf4f877938d22bf118c23b8f321f84274114
iZichtl <izichtl@gmail.com>
2021-08-27 17:44:02 -0300

    'text-blink'

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <router-view/>
 </template>
 <style lang="scss">
+@import 'src/styles/variables';
+@import url('https://fonts.googleapis.com/css2?family=Hina+Mincho&display=swap');
 *{
   margin: 0px;
   padding: 0px;
@@ -12,6 +14,16 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   height: 100vh;
+}
+h3 p{
+  font-family: 'Hina Mincho', serif;
+}
+a{
+  text-decoration: none;
+}
+a:visited{
+  text-decoration: none;
+  color: $preto;
 }
 
 </style>
